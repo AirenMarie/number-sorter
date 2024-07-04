babcdf0e3202ab1f1e996d7e489712f16a2a5570
AirenMarie <astringer92@gmail.com>
2024-07-04 13:20:04 -0400

    Create outer and inner for loops in selectionSort, create minIndex variable in inner for loop

--- number-sorter.js
+++ number-sorter.js
@@ -33,4 +33,10 @@ const bubbleSort = (array) => {
   return array;
 };
 
-const selectionSort = (array) => {};
+const selectionSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    let minIndex = i;
+
+    for (let j = i + 1; j < array.length; j++) {}
+  }
+};
